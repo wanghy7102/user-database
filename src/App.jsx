ac658086ccb73ccab3ca21f77057f66161df636c
Levine Wang <wang_levine@outlook.com>
2023-07-14 10:44:30 -0700

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import Layout from "./Layout";
 import Users from "./Users";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/user-database/users" element={<Users />} />
+        <Route path="/user-database/" element={<Layout />}>
+          <Route index element={<Users />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Layout.jsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import { Camera } from "@mui/icons-material";
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const defaultTheme = createTheme();
+
+const Layout = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Camera sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            User Database
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>{Outlet}</main>
+    </ThemeProvider>
+  );
+};
+
+export default Layout;
